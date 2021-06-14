99da7fc556508314bede1e638fad9de2f214a497
zz <zhangzhuo1024@163.com>
2021-06-14 22:24:40 +0800

    添加jni获取各类信息接口

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -44,3 +44,144 @@ Java_com_zz_ndkffmpeg_MainActivity_getFfmpegInfo(JNIEnv *env, jobject thiz) {
     return env->NewStringUTF(info);
 }
 }
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+JNIEXPORT jstring JNICALL
+Java_com_zz_ndkffmpeg_MainActivity_urlprotocolinfo(JNIEnv *env, jobject thiz) {
+
+    char info[40000] = {0};
+    av_register_all();
+
+    struct URLProtocol *pup = NULL;
+    //Input
+    struct URLProtocol **p_temp = &pup;
+    avio_enum_protocols((void **) p_temp, 0);
+    while ((*p_temp) != NULL) {
+        sprintf(info, "%s[In ][%10s]\n", info, avio_enum_protocols((void **) p_temp, 0));
+    }
+    pup = NULL;
+    //Output
+    avio_enum_protocols((void **) p_temp, 1);
+    while ((*p_temp) != NULL) {
+        sprintf(info, "%s[Out][%10s]\n", info, avio_enum_protocols((void **) p_temp, 1));
+    }
+
+    //LOGE("%s", info);
+    return env->NewStringUTF(info);
+}
+}
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+JNIEXPORT jstring JNICALL
+Java_com_zz_ndkffmpeg_MainActivity_avformatinfo(JNIEnv *env, jobject thiz) {
+    char info[40000] = {0};
+
+    av_register_all();
+
+    AVInputFormat *if_temp = av_iformat_next(NULL);
+    AVOutputFormat *of_temp = av_oformat_next(NULL);
+    //Input
+    while (if_temp != NULL) {
+        sprintf(info, "%s[In ][%10s]\n", info, if_temp->name);
+        if_temp = if_temp->next;
+    }
+    //Output
+    while (of_temp != NULL) {
+        sprintf(info, "%s[Out][%10s]\n", info, of_temp->name);
+        of_temp = of_temp->next;
+    }
+    //LOGE("%s", info);
+    return env->NewStringUTF(info);
+
+
+}
+}
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+JNIEXPORT jstring JNICALL
+Java_com_zz_ndkffmpeg_MainActivity_avcodecinfo(JNIEnv *env, jobject thiz) {
+    char info[40000] = {0};
+
+    av_register_all();
+
+    AVCodec *c_temp = av_codec_next(NULL);
+
+    while (c_temp != NULL) {
+        if (c_temp->decode != NULL) {
+            sprintf(info, "%s[Dec]", info);
+        } else {
+            sprintf(info, "%s[Enc]", info);
+        }
+        switch (c_temp->type) {
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s[Video]", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s[Audio]", info);
+                break;
+            default:
+                sprintf(info, "%s[Other]", info);
+                break;
+        }
+        sprintf(info, "%s[%10s]\n", info, c_temp->name);
+
+
+        c_temp = c_temp->next;
+    }
+    //LOGE("%s", info);
+
+    return env->NewStringUTF(info);
+
+
+}
+}
+
+extern "C"{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+JNIEXPORT jstring JNICALL
+Java_com_zz_ndkffmpeg_MainActivity_avfilterinfo(JNIEnv *env, jobject thiz) {
+    char info[40000] = {0};
+    avfilter_register_all();
+    AVFilter *f_temp = (AVFilter *) avfilter_next(NULL);
+//    while (f_temp != NULL) {
+        sprintf(info, "%s[%10s]\n", info, f_temp->name);
+//    }
+    //LOGE("%s", info);
+
+    return env->NewStringUTF(info);
+
+
+}
+}
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavcodec/jni.h>
+JNIEXPORT jstring JNICALL
+Java_com_zz_ndkffmpeg_MainActivity_configurationinfo(JNIEnv *env, jobject thiz) {
+    char info[10000] = {0};
+    av_register_all();
+
+    sprintf(info, "%s\n", avcodec_configuration());
+
+    //LOGE("%s", info);
+    return env->NewStringUTF(info);
+}
+}
--- app/src/main/java/com/zz/ndkffmpeg/MainActivity.java
+++ app/src/main/java/com/zz/ndkffmpeg/MainActivity.java
@@ -3,6 +3,8 @@ package com.zz.ndkffmpeg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.zz.ndkffmpeg.databinding.ActivityMainBinding;
@@ -24,8 +26,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText("ffmpeg info = " + getFfmpegInfo());
+        TextView libinfoText = binding.sampleText;
+
+
+        Button configurationButton = (Button) this.findViewById(R.id.button_configuration);
+        Button urlprotocolButton = (Button) this.findViewById(R.id.button_urlprotocol);
+        Button avformatButton = (Button) this.findViewById(R.id.button_avformat);
+        Button avcodecButton = (Button) this.findViewById(R.id.button_avcodec);
+        Button avfilterButton = (Button) this.findViewById(R.id.button_avfilter);
+
+
+        libinfoText.setText(configurationinfo());
+
+        urlprotocolButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                libinfoText.setText(urlprotocolinfo());
+            }
+        });
+
+        avformatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                libinfoText.setText(avformatinfo());
+            }
+        });
+
+        avcodecButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                libinfoText.setText(avcodecinfo());
+            }
+        });
+
+        avfilterButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                libinfoText.setText(avfilterinfo());
+            }
+        });
+
+        configurationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                libinfoText.setText(configurationinfo());
+            }
+        });
+
     }
 
     /**
@@ -35,4 +77,18 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
 
     public native String getFfmpegInfo();
+
+
+    //JNI
+    public native String urlprotocolinfo();
+
+    public native String avformatinfo();
+
+    public native String avcodecinfo();
+
+    public native String avfilterinfo();
+
+    public native String configurationinfo();
+
+
 }
